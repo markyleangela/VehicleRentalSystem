73746122dd18beeb84d8be1c625eb60ea65508b9
markyleangela <markyleangela@gmail.com>
2024-09-30 23:29:51 +0800

    Update landing page - Redirect landing page to login using form

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
+    if request.method == 'POST':
+        return redirect('login_page')
     return render(request, 'landing_page.html')
