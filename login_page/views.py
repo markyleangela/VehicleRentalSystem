085cc40fece93e6239a31d837c47955f915bdf8b
Chaz <chazzyl.llaguno@cit.edu>
2024-09-30 09:09:43 +0800

    Added the login authentication through username, and password

--- login_page/views.py
+++ login_page/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import LoginForm
 
@@ -8,8 +9,15 @@ def login_page(request):
             # Processing the data
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            # Add authentication logic here
-            return redirect('vehicle_list')  # Replace with your success URL
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                # Login the user
+                login(request, user)
+                return redirect('vehicle_list')
+            else:
+                # Return an error message if authentication fails
+                form.add_error(None, "Invalid username or password.")
     else:
         form = LoginForm()
 
