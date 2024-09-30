ac889bcacdc9d031c93644c28e401207a239909c
markyleangela <markyleangela@gmail.com>
2024-10-01 00:49:18 +0800

    Update landing page views - Redirect to register if register button is clicked

--- landing_page/views.py
+++ landing_page/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method == 'POST':
-        return redirect('login_page')
+        if request.POST.get('action') == 'login':
+            return redirect('login_page')
+        elif request.POST.get('action') == 'register':
+            return redirect('register')
     return render(request, 'landing_page.html')
 
