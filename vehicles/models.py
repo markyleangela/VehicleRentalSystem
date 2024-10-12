e0f93dac6627e974d6c7e8ff8ec35807d0a8e95b
Chaz <chazzyl.llaguno@cit.edu>
2024-10-12 20:29:36 +0800

    Implemented the create operation for the vehicles, and updated the vehicle model

--- vehicles/models.py
+++ vehicles/models.py
@@ -14,8 +14,12 @@ class Vehicle(models.Model):
     ]
     vehicle_id = models.AutoField(primary_key=True)
     vehicle_model = models.CharField(max_length=100)
-    vehicle_brand = models.CharField(max_length=100)
+    vehicle_brand = models.CharField(max_length=10)
     vehicle_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     vehicle_price = models.FloatField(default=0.00)
     vehicle_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Available")
+    vehicle_blobimage = models.BinaryField(null = True, blank=True)
+    vehicle_is_deleted = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.vehicle_model} {self.vehicle_brand} {self.vehicle_type} {self.vehicle_price} {self.vehicle_status}"
