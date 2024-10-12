415a2c58faf5a53f03d496d18fd190ee062fefbe
markyleangela <markyleangela@gmail.com>
2024-10-12 23:12:12 +0800

    Update vehicle list to show image in the list

--- vehicle_availability/views.py
+++ vehicle_availability/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+import base64
+from io import BytesIO
+from PIL import Image
+
 
 from vehicles.models import Vehicle
 
@@ -11,11 +15,30 @@ def vehicle_list(request, vehicle_id = None):
     if vehicle_type != 'all':
         vehicles = vehicles.filter(vehicle_type=vehicle_type)
 
+    for vehicle in vehicles:
+        if vehicle.vehicle_blobimage:
+          
+            image = Image.open(BytesIO(vehicle.vehicle_blobimage))
+            
+           
+            if image.mode == 'RGBA':
+                image = image.convert('RGB') 
+
+            buffer = BytesIO()
+            image_format = image.format
+            
+            if not image_format or image_format == 'JPEG':
+                image_format = 'JPEG'
+            image.save(buffer, format=image_format) 
+            mime_type = f"image/{image_format.lower()}"
+            vehicle.image_base64 = f"data:{mime_type};base64,{base64.b64encode(buffer.getvalue()).decode('utf-8')}"
+        else:
+            vehicle.image_base64 = None
+
 
     if request.method == 'POST':
         vehicle_id = request.POST.get('vehicle_id')  # Get the vehicle_id from the form submission
-        vehicle_detail = get_object_or_404(Vehicle, pk=vehicle_id)  # Get the specific vehicle details
-
+        vehicle_detail = get_object_or_404(Vehicle, pk=vehicle_id)  # Get the specific vehicle detail
 
 
         return render(request, 'vehicle_list/vehicle_list.html', {
