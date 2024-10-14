129b77c008057cbd31b0b5bb842eef4637bc5a60
Raphael Espelita <raphael.espelita@cit.edu>
2024-10-14 23:55:38 +0800

    Fixed registration error

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,8 @@ from .models import CustomUser
 class CustomUserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
 
-    # Customize fields similar to your previous form
     def __init__(self, *args, **kwargs):
         super(CustomUserRegistrationForm, self).__init__(*args, **kwargs)
         self.fields['username'].label = 'Username'
-        self.fields['email'].label = 'Email'
-        # Customize other fields as needed
