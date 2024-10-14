5210a454b8c0a28c63b133855bf2daa370627cfa
Raphael Espelita <raphael.espelita@cit.edu>
2024-10-15 00:35:15 +0800

    Updated forms.py and views.py in register app

--- register/forms.py
+++ register/forms.py
@@ -1,33 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from accounts.forms import CustomUserRegistrationForm
 
-class UserRegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
 
-    # Customizing fields to remove help_text and adjust labels
-    def __init__(self, *args, **kwargs):
-        super(UserRegistrationForm, self).__init__(*args, **kwargs)
-
-        # Customize the username field
-        self.fields['username'].label = 'Username'
-        self.fields['username'].help_text = ''  # Remove default help text
-        self.fields['username'].widget.attrs.update({
-            'placeholder': 'Enter Username'
-        })
-
-        # Customize the password1 field
-        self.fields['password1'].label = 'Password'
-        self.fields['password1'].help_text = ''  # Remove default help text
-        self.fields['password1'].widget.attrs.update({
-            'placeholder': 'Enter Password'
-        })
-
-        # Customize the password2 field
-        self.fields['password2'].label = 'Confirm Password'
-        self.fields['password2'].help_text = ''  # Remove default help text
-        self.fields['password2'].widget.attrs.update({
-            'placeholder': 'Confirm Password'
-        })
--- register/views.py
+++ register/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import UserRegistrationForm
 from django.contrib.auth import login
+from django.contrib import messages
+from accounts.forms import CustomUserRegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = CustomUserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful!")
             return redirect('home')
         else:
-            messages.error(request, "There was an error with your registration. Please check your details.")
-            print(form.errors)
+            messages.error(request, "There was an error with your registration.")
     else:
-        form = UserRegistrationForm()
+        form = CustomUserRegistrationForm()
 
     return render(request, 'register.html', {'form': form})
