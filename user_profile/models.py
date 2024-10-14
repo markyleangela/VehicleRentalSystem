945d568fd314b6f8b57f2fd97a9293eb53660134
markyleangela <markyleangela@gmail.com>
2024-10-14 19:43:47 +0800

    Update Profile models

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # first_name = models.CharField(max_length=30, blank=True)
-    # last_name = models.CharField(max_length=30, blank=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
     license_no = models.CharField(max_length=15, blank=True)
