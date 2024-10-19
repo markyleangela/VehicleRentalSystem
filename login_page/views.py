7f6e672dbaea5a89a5b7e9dc01924cfa59e0f0db
markyleangela <markyleangela@gmail.com>
2024-10-19 18:45:05 +0800

    Add nav bar to vehicle list page

--- login_page/views.py
+++ login_page/views.py
@@ -14,7 +14,7 @@ def login_page(request):
             if user is not None:
                 # Login the user
                 login(request, user)
-                return redirect('vehicle_list')
+                return redirect('home')
             else:
                 # Return an error message if authentication fails
                 form.add_error(None, "Invalid username or password.")
