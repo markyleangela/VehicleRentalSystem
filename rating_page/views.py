d82702695ac69e1cc89a69223ec75f0878fd05e9
markyleangela <markyleangela@gmail.com>
2024-11-10 22:53:30 +0800

    Update: change the view to make the user rate as long as the transaction is different

--- rating_page/views.py
+++ rating_page/views.py
@@ -1,41 +1,26 @@
-from django.shortcuts import render
-from vehicles.models import Vehicle
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import RatingForm
-
-import base64
-from io import BytesIO
-from PIL import Image
-import os
-from django.templatetags.static import static 
-from django.conf import settings
-# Create your views here.
-
 from django.shortcuts import render, redirect, get_object_or_404
 from vehicles.models import Vehicle
 from .forms import RatingForm
 from rental_record.models import RentalRecord
-
 from django.http import HttpResponse
 
 def vehicle_rating_detail(request, vehicle_id):
-    vehicle = get_object_or_404(Vehicle, vehicle_id=vehicle_id)  # Retrieve the vehicle by ID
+    # Retrieve the vehicle by ID
+    vehicle = get_object_or_404(Vehicle, vehicle_id=vehicle_id)
 
     # Fetch all ratings for the vehicle
     ratings = vehicle.ratings.all()
     avg_rating = vehicle.average_rating()
     count_ratings = vehicle.rating_count()
 
-    # Check if the user has rented this vehicle
-    rental_record = RentalRecord.objects.filter(customer=request.user, vehicle=vehicle).first()
+    # Find an unrated rental record for this user and vehicle, if any
+    rental_record = RentalRecord.objects.filter(customer=request.user, vehicle=vehicle, is_rated=False).first()
 
-    # If no rental record exists, the user can't rate the vehicle
-   
+    # If no rental record exists, inform the user that they can't rate the vehicle
+    if not rental_record:
+        return HttpResponse("You need to rent this vehicle before you can leave a rating.")
 
-    # Check if the user has already rated the vehicle (you can add a condition to prevent double-rating)
-    if rental_record.is_rated:
-        return HttpResponse("You already rated this car")
-
+    # Handle form submission for rating
     if request.method == 'POST':
         form = RatingForm(request.POST)
         if form.is_valid():
@@ -56,7 +41,6 @@ def vehicle_rating_detail(request, vehicle_id):
     full_stars = int(avg_rating)  # Full stars (e.g., 4 for 4.2)
     half_star = 1 if avg_rating - full_stars >= 0.5 else 0  # Half star if there's any fraction
     empty_stars = 5 - full_stars - half_star  # Remaining stars are empty
-
     stars = ['full'] * full_stars + ['half'] * half_star + ['empty'] * empty_stars
 
     context = {
@@ -69,4 +53,3 @@ def vehicle_rating_detail(request, vehicle_id):
     }
 
     return render(request, 'vehicle_rating.html', context)
-
